e49e90b043ad80c6b8842f4ed05c846101495cdb
jackbuzzhealy <jackbuzzhealy001@outlook.com>
2020-08-03 14:58:20 +0000

    models modified

--- application/models.py
+++ application/models.py
@@ -1,21 +1,21 @@
 from application import db
 from flask_login import UserMixin
 
-class tblGolfer(db.Model, UserMixin):
+class Golfer(db.Model, UserMixin):
     email = db.Column(db.String(50), primary_key=True)
     foreName = db.Column(db.String(25), nullable=False, unique=True)
     secondName = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
 
-class tblTimeSlots(db.Model):
+class TimeSlots(db.Model):
     timeID = db.Column(db.Integer, primary_key=True)
     slot = db.Column(db.String(25), nullable=False)
 
-class tblBooking(db.Model):
+class Booking(db.Model):
      bookingID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-     email = db.Column(db.String(50), db.ForeignKey('tblGolfer.email'), nullable=False)
+     email = db.Column(db.String(50), db.ForeignKey('golfer.email'), nullable=False)
 
-class tblBookingLine(db.Model):
+class BookingLine(db.Model):
      lineID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-     bookingID = db.Column(db.Integer, db.ForeignKey('tblBooking.bookingID'), nullable=False)
-     timeID = db.Column(db.Integer, db.ForeignKey('tblTimeSlots.timeID'),nullable=False)
+     bookingID = db.Column(db.Integer, db.ForeignKey('booking.bookingID'), nullable=False)
+     timeID = db.Column(db.Integer, db.ForeignKey('time_slots.timeID'),nullable=False)

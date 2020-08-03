ad912dc12d37094b56a8bc08a238b0d6f2713e01
Jack Healy <68054142+jackbuzzhealy@users.noreply.github.com>
2020-08-03 14:25:37 +0100

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,17 @@
-# import Flask class from the flask module
 from flask import Flask
-# create a new instance of Flask and store it in app 
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
 app = Flask(__name__)
-# import the ./application/routes.py file
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 from application import routes

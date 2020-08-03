f959d9621e1b475cf3c6aa5f217101b343220831
jackbuzzhealy <jackbuzzhealy001@outlook.com>
2020-08-03 15:33:16 +0000

    16.33, 03.08.2020

--- application/__init__.py
+++ application/__init__.py
@@ -5,9 +5,6 @@ from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
-from application import app
+from application import app, db
+from application.models import Golfer, TimeSlots, Booking, BookingLine
 
 @app.route('/')
 @app.route('/home')
